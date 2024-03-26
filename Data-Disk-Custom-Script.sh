baaa50a89589420a24ac27e59c4dada689c7b6db
ibanfernandezchico <126248894+ibanfernandezchico@users.noreply.github.com>
2024-03-26 14:33:57 +0100

    Data-Disk-Custom-Script.sh

--- Data-Disk-Custom-Script.sh
+++ Data-Disk-Custom-Script.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# Obtener el nombre del grupo de recursos
-resourceGroupName=$(az vm list --query "[0].resourceGroup" -o tsv)
-
-# Obtener el ID del disco de datos
-diskId=$(az vm show -g $resourceGroupName -n $(az vm list --query "[0].name" -o tsv) --query "storageProfile.dataDisks[0].managedDisk.id" -o tsv)
+# Ubicación del disco de datos
+diskPath="/dev/disk/azure/scsi1/lun0"
 
 # Formatear el disco de datos
-sudo mkfs.ext4 $diskId
+sudo mkfs.ext4 $diskPath
 
 # Crear un punto de montaje para el disco de datos
-sudo mkdir /mnt/datos
+sudo mkdir /mnt/data
 
 # Montar el disco de datos
-sudo mount $diskId /mnt/datos
+sudo mount $diskPath /mnt/data
 
 # Actualizar /etc/fstab para montar el disco de datos automáticamente en el arranque
-echo "$diskId /mnt/datos ext4 defaults,nofail 0 2" | sudo tee -a /etc/fstab
+echo "$diskPath /mnt/data ext4 defaults,nofail 0 2" | sudo tee -a /etc/fstab
