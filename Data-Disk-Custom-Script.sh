60d7a8b0dec76b7b568f15a8be650074f0da3d35
ibanfernandezchico <126248894+ibanfernandezchico@users.noreply.github.com>
2024-03-26 14:22:59 +0100

    Data-Disk-Custom-Script.sh

--- Data-Disk-Custom-Script.sh
+++ Data-Disk-Custom-Script.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Obtener el nombre del grupo de recursos de la VM
-resourceGroupName=$(az vm show -g $(az vm list --query "[?name=='SMC-VM01-Alastria'].resourceGroup" -o tsv) -n SMC-VM01-Alastria --query "resourceGroup" -o tsv)
+# Obtener el nombre del grupo de recursos
+resourceGroupName=$(az vm list --query "[0].resourceGroup" -o tsv)
 
 # Obtener el ID del disco de datos
-diskId=$(az vm show -g $resourceGroupName -n SMC-VM01-Alastria --query "storageProfile.dataDisks[0].managedDisk.id" -o tsv)
+diskId=$(az vm show -g $resourceGroupName -n $(az vm list --query "[0].name" -o tsv) --query "storageProfile.dataDisks[0].managedDisk.id" -o tsv)
 
 # Formatear el disco de datos
 sudo mkfs.ext4 $diskId
 
 # Crear un punto de montaje para el disco de datos
-sudo mkdir /mnt/data
+sudo mkdir /mnt/datos
 
 # Montar el disco de datos
-sudo mount $diskId /mnt/data
+sudo mount $diskId /mnt/datos
 
 # Actualizar /etc/fstab para montar el disco de datos automáticamente en el arranque
-echo "$diskId /mnt/data ext4 defaults,nofail 0 2" | sudo tee -a /etc/fstab
+echo "$diskId /mnt/datos ext4 defaults,nofail 0 2" | sudo tee -a /etc/fstab
