d05c8cbaa2f32f5543fbff8d9fa6a0e1695b2c17
ibanfernandezchico <126248894+ibanfernandezchico@users.noreply.github.com>
2024-03-26 14:19:48 +0100

    Data-Disk-Custom-Script.sh

--- Data-Disk-Custom-Script.sh
+++ Data-Disk-Custom-Script.sh
@@ -10,10 +10,10 @@ diskId=$(az vm show -g $resourceGroupName -n SMC-VM01-Alastria --query "storageP
 sudo mkfs.ext4 $diskId
 
 # Crear un punto de montaje para el disco de datos
-sudo mkdir /mnt/datadisk
+sudo mkdir /mnt/data
 
 # Montar el disco de datos
-sudo mount $diskId /mnt/datadisk
+sudo mount $diskId /mnt/data
 
 # Actualizar /etc/fstab para montar el disco de datos automáticamente en el arranque
-echo "$diskId /mnt/datadisk ext4 defaults,nofail 0 2" | sudo tee -a /etc/fstab
+echo "$diskId /mnt/data ext4 defaults,nofail 0 2" | sudo tee -a /etc/fstab
